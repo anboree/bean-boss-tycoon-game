6837572d9a95aa5709f815011419a6fa7ea7f0fc
anboree <zakisdavis423@gmail.com>
2026-01-05 09:49:19 +0200

    Added game container and sub-containers for the game and link to JS file

--- index.php
+++ index.php
@@ -18,6 +18,31 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <!-- Game Container -->
+    <div class="gameContainer">
+
+        <!-- Business Name -->
+        <p class="business-name"><span id="businessName"><b>My Coffee Stand</b></span></p>
+
+        <!-- Day & Time -->
+        <div class="day-time">
+            <p>Day: <span id="currentDay">1</span></p>
+            <p>Time: <span id="currentTime">7:00 AM</span></p>
+        </div>
+
+        <!-- Main game box -->
+        <div class="main-box">
+            <p>Money: $<span id="money">0</span></p>
+            <button id="brewBtn">Brew Coffee</button>
+        </div>
+
+        <!-- Upgrades box -->
+        <p id="upgradesHeaderText">Upgrades</p>
+        <div class="upgrades-box">
+        </div>
+
+    </div>
+
+    <script src="js/app.js"></script>
 </body>
 </html>
