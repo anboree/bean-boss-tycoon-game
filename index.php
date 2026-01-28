c1d653d82be111f0dc9de2a5bd023abe97b32053
anboree <zakisdavis423@gmail.com>
2026-01-28 17:10:40 +0200

    Changed condition and updated include functions

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
     session_start();
 
-    if(isset($_SESSION["id"])){
-        include("navbar-logged-in.html");
-    }
-    else{
+    include("db/connection.php");
+    include("navbar.php");
+
+    if(!isset($_SESSION["id"])){
         header("Location: welcome.php");
     }
 ?>
