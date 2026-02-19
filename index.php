faaabbd6c6b8014788506286a560b548d22015c7
anboree <zakisdavis423@gmail.com>
2026-02-19 13:30:53 +0200

    Cleaned up code and added rope decorations

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
     session_start();
 
-    include("db/connection.php");
-    include("navbar.php");
-
     if(!isset($_SESSION["id"])){
         header("Location: welcome.php");
     }
+
+    include("db/connection.php");
+    include("navbar.php");
 ?>
 
 <!DOCTYPE html>
@@ -15,21 +15,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bean Boss</title>
-    
-    <!-- Imported fonts from Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <!-- Font API -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Press+Start+2P&display=swap" rel="stylesheet">
 
     <!-- Favicon -->
     <link rel="icon" type="image/png" href="assets/bean-boss-favicon.png">
-
-    <!-- Link to CSS file -->
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <!-- Game Container -->
     <div class="gameContainer">
+
+        <!-- Added Decoration Ropes that visually holds up container -->
+        <span class="rope-decor-left"></span>
+        <span class="rope-decor-right"></span>
 
         <!-- Business Name -->
         <p class="business-name"><span id="businessName"><b>My Coffee Stand</b></span></p>
