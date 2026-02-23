285d402d4ce98f040eb4cb83c8da13bc5d5f7174
anboree <zakisdavis423@gmail.com>
2026-02-23 15:49:26 +0200

    Added SQL query for profile pictures

--- navbar.php
+++ navbar.php
@@ -3,6 +3,20 @@
     if(session_status() === PHP_SESSION_NONE){
         session_start();
     }
+
+    include("db/connection.php");
+
+    $stmt = $conn->prepare("
+        SELECT user_account_details.profile_picture
+        FROM user_account_details
+        WHERE user_account_details.user_id = ?
+    ");
+
+    $stmt->bind_param("i", $_SESSION["id"]);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +24,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Imported fonts from Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Press+Start+2P&display=swap" rel="stylesheet">
+
+    <!-- Link to CSS file -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <nav class="navbar">
@@ -27,13 +49,15 @@
                     <a href="leaderboard.php" class="nav-link leaderboard-link">Leaderboard</a>
 
                     <a href="user_account.php" class="nav-link">
-                        <img id="profile-picture" src="assets/default-pfp.jpg" alt="Profile">
+                        <img id="profile-picture" src="profile_pictures/<?php echo htmlspecialchars($user['profile_picture'] ?? 'default-pfp.jpg'); ?>" alt="Profile">
                     </a>
                 </div>
             <?php else: ?>
                 <!-- Not logged-in user -->
-                <a href="login.php" class="nav-link">&#128273; Login</a>
-                <a href="register.php" class="nav-link">&#128203; Register</a>
+                <div class="register-login-nav-div">
+                    <img src="assets/login-icon.png" width="20px" style="margin-top: -3px; margin-right: 3px;" alt="login-icon"><a href="login.php" class="nav-link"> Login</a>
+                    <img src="assets/register-icon.png" width="20px" style="margin-top: -5px; margin-right: 2px;" alt="register-icon"><a href="register.php" class="nav-link"> Register</a>
+                </div>
             <?php endif; ?>
         </div>
     </nav>
